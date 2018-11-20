ddff3f7bec305795b5fd03fffb1827eead209035
Zhang.Cong <1084823834@qq.com>
2018-11-20 23:53:51 +0800

    完成进入动画

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app" class="absolute-fill-parent flex-display flex-center">
+
+
+        <div class="front-page flex-display">
+            <div class="line-part top-part">
+            </div>
+            <div class="line-part bottom-part"></div>
+            <div class="avatar flex-display flex-center">
+                <img src="./assets/images/avatar.jpg" alt="Avatar">
+            </div>
+            <p class="desc"><span>I'm </span><span ref="descStr"></span></p>
+        </div>
+        <!--<div id="nav">-->
+        <!--<router-link to="/">Home</router-link> |-->
+        <!--<router-link to="/about">About</router-link>-->
+        <!--</div>-->
+        <!--<router-view/>-->
     </div>
-    <router-view/>
-  </div>
 </template>
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
-    import {TweenLite, CSSPlugin, TimelineLite} from 'gsap/TweenMax'
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {TweenLite, CSSPlugin, TimelineLite} from 'gsap/TweenMax';
+    import {init} from 'ityped';
 
     @Component
     export default class App extends Vue {
         private mounted() {
-            let tl = new TimelineLite()
-            let loaderWrapper = document.getElementById('loaderWrapper')
-            let preLoader = document.getElementById('preLoader')
-            let circle = document.getElementById('circle')
+            const tl = new TimelineLite();
+            const loaderWrapper = document.getElementById('loaderWrapper');
+            const preLoader = document.getElementById('preLoader');
+            const circle = document.getElementById('circle');
+            const descStr = this.$refs.descStr
             tl.to(circle, 0.3, {
                 opacity: 0,
             }).to(preLoader, 0.5, {
                 height: 300,
-                width: 300,
+                width: 500,
             }).to(loaderWrapper, 0.5, {
                 opacity: 0,
-                // delay: 1
             }).to(loaderWrapper, 0.5, {
-                display: 'none'
-            })
+                display: 'none',
+            });
+            init(descStr, {showCursor: true, strings: ['Collin.', 'a web developer.']})
+
             // new TweenLite.to(circle, 0.5, {
             //     opacity: 0,
             // })
@@ -46,18 +60,75 @@
     }
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+    #app
+        z-index 50
+        background: url("assets/images/main-bg.jpg") center bottom
+        background-size cover
+        .front-page
+            position relative
+            padding 20px
+            width 500px
+            height 300px
+            background-color: #0d0d0d
+            flex-direction column
+            align-items center
+            border-radius 5px
+            overflow hidden
+            &::before
+                position absolute
+                content ''
+                width 50%
+                top 50%
+                left 0
+                height 5px
+                background-color: #f4d03f
+                transform-origin right bottom
+                transform rotateZ(20deg)
+            &::after
+                position absolute
+                content ''
+                width 50%
+                top 50%
+                right 0
+                height 5px
+                background-color: #f4d03f
+                transform-origin left bottom
+                transform rotateZ(-20deg)
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+            .avatar
+                width 100%
+                img
+                    width 100px
+                    height 100px
+                    border-radius 50%
+            .desc
+                color #fff
+                letter-spacing 3px
+                padding-top 50px
+                font-size 2rem
+                font-weight 700
+                .ityped-cursor {
+                    font-size: 2.2rem;
+                    opacity: 1;
+                    -webkit-animation: blink 0.4s infinite;
+                    -moz-animation: blink 0.4s infinite;
+                    animation: blink 0.4s infinite;
+                    animation-direction: alternate;
+                }
+
+                @keyframes blink {
+                    100% {
+                        opacity: 0;
+                    }
+                }
+                @-webkit-keyframes blink {
+                    100% {
+                        opacity: 0;
+                    }
+                }
+                @-moz-keyframes blink {
+                    100% {
+                        opacity: 0;
+                    }
+                }
 </style>
